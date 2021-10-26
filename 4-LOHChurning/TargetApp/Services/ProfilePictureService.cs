60028fcbb82e1bb1337c612238b1ecd3ce992ddd
Mike Rousos <Mike@MJRousos.com>
2021-10-25 23:13:54 -0400

    Add sln for scenario 4

--- 4-LOHChurning/TargetApp/Services/ProfilePictureService.cs
+++ 4-LOHChurning/TargetApp/Services/ProfilePictureService.cs
@@ -11,8 +11,8 @@ namespace TargetApp.Services
             // This would normally retrieve a profile picture from storage
             // For a simpler demo, this will just get 100k random bytes, instead.
             var ret = CreateProfilePicture(100 * 1024);
-            //var random = new Random();
-            //random.NextBytes(ret.Bytes);
+            var random = new Random();
+            random.NextBytes(ret.Bytes);
             return ret;
         }
     }
