8eada65ad00c963944d7cb8880a1106119e05216
Mike Rousos <Mike@MJRousos.com>
2021-10-27 09:22:45 -0400

    Put Account type in a namespace

--- 5-Deadlock/TargetApp/Account.cs
+++ 5-Deadlock/TargetApp/Account.cs
@@ -1,13 +1,16 @@
-public class Account
+namespace TargetApp
 {
-    public decimal Balance { get; set; }
-
-    // This is a bad architecture. Do not do this.
-    public SemaphoreSlim SynchronizationSemaphore { get; set; }
-
-    public Account(decimal initialBalance)
+    public class Account
     {
-        Balance = initialBalance;
-        SynchronizationSemaphore = new SemaphoreSlim(1, 1);
+        public decimal Balance { get; set; }
+
+        // This is a bad architecture. Do not do this.
+        public SemaphoreSlim SynchronizationSemaphore { get; set; }
+
+        public Account(decimal initialBalance)
+        {
+            Balance = initialBalance;
+            SynchronizationSemaphore = new SemaphoreSlim(1, 1);
+        }
     }
 }
